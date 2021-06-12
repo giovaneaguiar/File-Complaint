53520cf8078b0a3e3cdde965c5752ff03c690afc
Giovane <giovaneaguiar@ice.ufjf.br>
2021-06-12 16:38:19 -0300

    estiliza formulário.

--- formulario/src/App.vue
+++ formulario/src/App.vue
@@ -1,31 +1,70 @@
 <template>
   <div id="app">
-      <Rotulo/>
-      <Escolha/>
+    <h1>Registrar Reclamação</h1>
+    <div class="conteudo">
+      <form class="painel">
+        <div class="cabecalho">Formulário</div>
+        <Rotulo nome="E-mail">
+          <input type="text" />
+        </Rotulo>
+        <Escolha></Escolha>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import Rotulo from './components/Rotulo.vue'
-import Escolha from './components/Escolha.vue'
-
+import Rotulo from "./components/Rotulo.vue";
+import Escolha from "./components/Escolha.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Rotulo, Escolha
-   
-  }
-}
+    Rotulo,
+    Escolha,
+  },
+};
 </script>
 
 <style>
+body {
+  background: #dae2f8;
+  background: -webkit-linear-gradient(to right, #d6a4a4, #dae2f8);
+  background: linear-gradient(to right, #d6a4a4, #dae2f8);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
   margin-top: 60px;
+}
+
+.conteudo {
+  display: flex;
+}
+.painel {
+  flex: 1;
+  background: #fff;
+  margin: 0px 10px;
+  padding: 20px;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+}
+
+.painel .cabecalho {
+  width: 100%;
+  background-color: #ddd;
+  padding: 10px 0px;
+  border-radius: 5px;
+  font-size: 1.4rem;
+}
+
+#app h1 {
+  font-weight: 300;
+  margin: 20px;
+  padding: 0;
 }
 </style>
--- formulario/src/components/Rotulo.vue
+++ formulario/src/components/Rotulo.vue
@@ -1,13 +1,45 @@
 <template>
-  <h1>Rótulo</h1>
+  <div class="rotulo">
+        <label>{{ nome }}:</label>
+        <div class="elemento">
+            <slot></slot>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
+    props: ['nome']
 
 }
 </script>
 
-<style>
+<style scoped>
+.rotulo {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin: 15px 0px;
+        font-size: 1.4rem;
+    }
+    .elemento {
+        display: flex;
+        align-self: stretch;
+        margin: 10px 0px;
+    }
+
+    input, textarea, select {
+        flex: 1;
+        font-size: 1.4rem;
+        outline: none;
+        border: 1px solid #DDD;
+        border-radius: 5px;
+        padding: 5px 10px;
+    }
+
+
+    input:focus {
+        border: 1px solid #BBB;
+    }
 
 </style>
