eb354c44be6278c5a362f76905d1de98a3f611ef
Giovane <giovaneaguiar@ice.ufjf.br>
2021-06-14 11:33:08 -0300

    usando <textarea> e preservando quebras de linha.

--- formulario/src/App.vue
+++ formulario/src/App.vue
@@ -14,7 +14,7 @@
           <input type="number" v-model.number="usuario.idade"/>
         </Rotulo>
         <Rotulo nome="Mensagem">
-          <textarea name="" cols="20" rows="5"></textarea>
+          <textarea name="" cols="20" rows="5" v-model="mensagem"></textarea>
         </Rotulo>
         <Rotulo nome="Características do Problema">
           <span class="mr-4"
@@ -52,7 +52,9 @@
           <span>{{ usuario.idade }}</span>
         </Rotulo>
         <Rotulo nome="Mensagem">
-          <span>???</span>
+          <span style="white-space: pre;">{{ mensagem }}</span>
+          <!-- adicionei um estilo no span para preservar os espaços
+          em branco no textarea. -->
         </Rotulo>
         <Rotulo nome="Marque as Opções">
           <span>???</span>
@@ -83,6 +85,7 @@ export default {
   },
   data(){
     return {
+      mensagem: '',
       usuario: {
         email:'',
         senha: '',
